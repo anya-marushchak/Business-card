9b0f7b60a220a473ee8c2bd57bf7b2f2c8c05dd7
Anna Marushchak <maruschakanya86@gmail.com>
2023-01-31 21:06:19 +0100

    Edit file

---
+++ Business-card/mypage.py
@@ -0,0 +1,20 @@
+from flask import render_template,Flask, request,redirect
+
+
+app = Flask(__name__)
+
+from flask import render_template
+
+@app.route('/mypage/me', methods=['GET', 'POST'])
+def mypage():
+   if request.method == 'GET':
+       print("We received GET")
+       return render_template("child-1.html")
+   elif request.method == 'POST':
+       print("We received POST")
+       print(request.form)
+       return redirect("/")
+       
+
+if __name__ == "__main__":
+    app.run()

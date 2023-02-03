5dcf8ae1c5d86fce788ce154733ed69cf470355e
Anna Marushchak <maruschakanya86@gmail.com>
2023-02-03 15:57:33 +0100

    Edit file

--- Currency/main.py
+++ Currency/main.py
@@ -2,3 +2,26 @@ import requests
 
 response = requests.get("http://api.nbp.pl/api/exchangerates/tables/C?format=json")
 data = response.json()
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+user = {"username": "jamesbond", "password": "superpass123"}
+
+
+@app.route("/login/", methods=["GET", "POST"])
+def login():
+  if request.method == "POST":
+    data = request.form
+    username = data.get('username')
+    password = data.get("password")
+
+    if username == user['username'] and password == user["password"]:
+      return " Ти вже в системі. Повідомлення самознищиться... "
+
+  return render_template("login_form_post.html")
+
+
+if __name__ == "__main__":
+  app.run(debug=True)

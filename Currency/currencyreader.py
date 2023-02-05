d5ba341064f02042deeb711741fee1f3eb7b5800
Anna Marushchak <maruschakanya86@gmail.com>
2023-02-05 17:23:57 +0100

    Edit file

--- Currency/currencyreader.py
+++ Currency/currencyreader.py
@@ -1,3 +1,4 @@
+
 import requests
 
 response = requests.get("http://api.nbp.pl/api/exchangerates/tables/C?format=json")
@@ -7,9 +8,10 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-import csv
-with open('rates.csv', newline="") as csvfile:
-    rates = csv.reader(csvfile)
+
+import os
+
+os.path.join(os.getcwd(), 'rates.csv')
 
 
 @app.route("/currency", methods=["GET", "POST"])
@@ -19,11 +21,21 @@ def login():
     code = data.get('code')
     amount = data.get("amount")
 
-    if code == code['USD']:
+    
+
+    if code == code['USD'] and amount == 1:
       return amount*4.2534
 
-  return render_template("rates.html")
+    return render_template("rates.html")
+  
 
 
 if __name__ == "__main__":
   app.run(debug=True)
+
+
+
+
+    
+  
+    

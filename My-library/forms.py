f3a9ced763ee0204629d32fae0f34330002cbca3
Anna Marushchak <maruschakanya86@gmail.com>
2023-02-08 18:22:06 +0100

    Edit file

--- My-library/forms.py
+++ My-library/forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
-from wtforms.validators import DataRequired
 
-class LoginForm(FlaskForm):
-  username = StringField('username', validators=[DataRequired()])
-  password = PasswordField('password', validators=[DataRequired()])
+from forms import TodoForm
+
+
+
+class TodoForm(FlaskForm):
+  form = TodoForm()
